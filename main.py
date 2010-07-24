20555342feb7b5a04997293e48000600aaef3b99
jessykate <jessy.cowansharp@gmail.com>
2010-07-24 13:01:02 -0400

    more buildout

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import tornado.escape
 import urllib, urllib2
 import os, datetime
 try:
@@ -26,30 +27,30 @@ DATABASE_NAME = 'wodtracker'
 DATETIME_FORMAT_STRING = "%A, %B %d %Y %H:%M:%S EST"
 
 
-class MainHandler(tornado.web.RequestHandler):
+class NewWODHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('templates/index.html')
 
-class FormHandler(tornado.web.RequestHandler):
+class WODSubmitHandler(tornado.web.RequestHandler):
     def get(self):
         # arguments is a dict of key:value pairs where each value is a
         # list (even if there is only one item). 
         name = self.get_argument("name", None)
         wod = self.get_argument("wod", None)
-        score = self.get_argument("score", None)
-        comments = self.get_argument("comments", None)
+        date = self.get_argument("date", datetime.datetime.now())
+        tags = self.get_argument("tags", None)
         equipment_required = bool(self.get_argument("equipment", True))
-        date = datetime.datetime.now()
 
-        if not wod or not name or not score:
+        if not wod and name:
             print 'missing fields'
             return
 
         # the date object is automagically converted to the proper
         # bson datetime format, so we can still do things like sort by
         # time.
-        new_record = {'date': date, 'name': name, 'wod': wod, 'score': score, 
-                      'comments': comments, 'equipment_required': equipment_required}
+	# XXX need to ensure slug is unique
+        new_record = {'date': date, 'name': name, 'wod': wod, 'tags': tags, 
+                      'equipment_required': equipment_required}
 
         try:
             record_id = wod_save(new_record)
@@ -58,16 +59,44 @@ class FormHandler(tornado.web.RequestHandler):
             self.render('templates/thanks.html', message=message)
             return
 
-        # get all the workouts for the person with this name
-        username = get_name_from_id(record_id)
-        print 'retrieving user info:'
-        print 'name = %s' % username 
-        self.redirect("/%s" % username)
+	# define a slug for this WOD as either the WOD name or its unique ID in
+	# the database. 
+	slug = (name and tornado.escape.url_escape(name)) or str(record_id)
+	#print slug
+	#print type(slug)
+	db = pymongo.Connection()[DATABASE_NAME]
+	table = db.wods
+	table.update({'_id': record_id}, {"$set": {'slug': slug}})
+
+        # go to this workout's page.  
+        print 'slug = %s' % slug 
+        self.redirect("/wod/%s" % slug)
 
 class UserPageHandler(tornado.web.RequestHandler):
     def get(self, username):
         workouts = get_workouts_by_user(username)
         self.render("templates/user.html", workouts=workouts, user=username)
+
+class WODDisplayHandler(tornado.web.RequestHandler):
+    def get(self, slug):
+	wodinfo = wod_retrieve('slug', slug)
+	# XXX should do some error handling here if wod DNE etc. 
+	self.render("templates/wod.html", wodinfo = wodinfo)
+
+class WODScoreHandler(tornado.web.RequestHandler):
+    def get(self, slug):
+	wodinfo = wod_retrieve('slug', slug)
+	self.render("templates/score.html", wodinfo=wodinfo)
+
+class WODScoreSubmitHandler(tornado.web.RequestHandler):
+    def post(self):
+	pass
+
+def wod_retrieve(k,v):
+    ''' retrive a single WOD record based on the key 'k' and value 'v' '''
+    db = pymongo.Connection()[DATABASE_NAME]
+    table = db.wods
+    return table.find_one({k : v}) 	
 
 def wod_save(record):
     db = pymongo.Connection()[DATABASE_NAME]
@@ -98,12 +127,14 @@ settings = {
 }
 
 application = tornado.web.Application([
-        (r'/', MainHandler),
-        (r'/submit', FormHandler),
+        (r'/new', NewWODHandler),
+        (r'/wod_submit', WODSubmitHandler),
+	(r'/wod/(\w+)/score', WODScoreHandler),
+	(r'/wod/(\w+)', WODDisplayHandler),
         (r'/(\w*)', UserPageHandler),
         ], **settings)
 
 if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(8123)
     tornado.ioloop.IOLoop.instance().start()
